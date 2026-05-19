32a2aea86bce67ddb02cc8e0ddb7052b6fe8dc2a
Alfredo Sebastian Vallejo Aguado <alfredo.vallejo@st.ovgu.de>
2026-05-19 12:21:09 +0200

    Change in the Font: Nunito

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,21 @@
 ﻿import type { Metadata } from "next";
+import { Nunito, Playfair_Display } from "next/font/google";
 import "./globals.css";
 
 import { SiteFooter } from "@/components/layout/site-footer";
 import { SiteHeader } from "@/components/layout/site-header";
+
+const nunito = Nunito({
+  subsets: ["latin"],
+  variable: "--font-nunito",
+  weight: ["400", "500", "600", "700", "800"],
+});
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+  weight: ["600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "MiCuento | Personalisierte Kindergeschichten",
@@ -17,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="de">
-      <body>
+      <body className={`${nunito.variable} ${playfair.variable}`}>
         <SiteHeader />
         <main className="min-h-screen">{children}</main>
         <SiteFooter />
